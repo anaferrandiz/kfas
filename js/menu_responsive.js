ddb92051f6c71a9828cd40b1ab9e1f971276df58
Ana Ferrandiz Pascual <anaferrandizpascual@MacBook-Pro-de-Ana.local>
2025-10-30 11:15:53 +0100

    accesibility

--- js/menu_responsive.js
+++ js/menu_responsive.js
@@ -1,12 +1,17 @@
 'use strict'
 
+// ************* MENÚ RESPONSIVE *************
 
-// ************* MENÚ RESPONSIVE
 const headerBtn = document.querySelector('.header__btn')
 const headerNav = document.querySelector('.header__nav')
 
-headerBtn.addEventListener('click', function(){
-    headerNav.classList.toggle('isActive')
-   })
-// ************* FIN MENÚ RESPONSIVE
+// Inicializamos el estado aria-expanded
+headerBtn.setAttribute('aria-expanded', 'false')
 
+headerBtn.addEventListener('click', function () {
+  const isActive = headerNav.classList.toggle('isActive')
+  // Cambiamos el aria-expanded según el estado
+  headerBtn.setAttribute('aria-expanded', isActive)
+})
+
+// ************* FIN MENÚ RESPONSIVE *************
